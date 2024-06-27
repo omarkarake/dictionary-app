b365819ccb22697e117d1129466c188abe3d8484
omar karake <omar.karake@amalitechtraining.org>
2024-06-27 21:06:56 +0200

    adding effect on dark mode when toggled

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,15 @@
 import "./App.css";
 import Body from "./components/Body";
 import Header from "./components/Header";
-import { ThemeProvider } from "./context/ThemeContext";
 
 function App() {
   return (
-    <ThemeProvider>
-      <div className="w-screen flex items-center justify-center dark:bg-dark">
-        <div className="w-[90vw] md:w-[88vw] lg:w-[60vw] py-2 md:py-4 lg:py-8 bg-lighter dark:bg-dark">
-          <Header />
-          <Body />
-        </div>
+    <div className="w-screen flex items-center justify-center dark:bg-dark">
+      <div className="w-[90vw] md:w-[88vw] lg:w-[60vw] py-2 md:py-4 lg:py-8 bg-lighter dark:bg-dark">
+        <Header />
+        <Body />
       </div>
-    </ThemeProvider>
+    </div>
   );
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,12 +8,12 @@ const Header = () => {
 
   return (
     <>
-      <div>
+      <div className="mt-6 md:mt-14">
         <div className="flex justify-between items-center">
-          <RiBook2Line className="w-7 h-8 md:w-8 md:h-9" />
+          <RiBook2Line className="w-7 h-8 md:w-8 md:h-9 dark:text-gray" />
           <div className="flex items-center">
             <div className="flex justify-center items-center border-r-2 border-r-solid border-r-lightGray pr-3 md:pr-5">
-              <p className="font-sans text-body-s md:text-body-m">Sans Serif</p>
+              <p className="font-sans text-body-s md:text-body-m dark:text-lighter">Sans Serif</p>
               <RiArrowDownSLine className="w-6 h-6 text-purple ml-3 md:ml-5" />
             </div>
             <div className="ml-2 md:ml-5" onClick={toggleTheme}>
@@ -22,10 +22,10 @@ const Header = () => {
                   theme === "dark" ? "bg-purple items-end pr-1" : "bg-gray pl-1"
                 }`}
               >
-                <div className="w-[14px] h-[14px] bg-lighter rounded-full cursor-pointer"></div>
+                <div className="w-[14px] h-[14px] bg-lighter rounded-full cursor-pointer "></div>
               </div>
             </div>
-            <IoMoonOutline className="w-5 h-5 ml-3 md:ml-5" />
+            <IoMoonOutline className="w-5 h-5 ml-3 md:ml-5 dark:text-purple" />
           </div>
         </div>
       </div>
