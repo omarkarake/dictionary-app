eca4c04d75b183fba21ae9c8d799e2340604d24e
omar karake <omar.karake@amalitechtraining.org>
2024-06-28 17:17:02 +0200

    update darkMode state according to the user system background dark/light mode viceversa

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,11 @@ import Body from "./components/Body";
 import Header from "./components/Header";
 
 function App() {
+  // Check system preference for dark mode
+  const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
   const [selectedFont, setSelectedFont] = useState("Sans Serif");
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(prefersDarkMode);
 
   const handleFontChange = (font: string) => {
     setSelectedFont(font);
@@ -14,6 +17,19 @@ function App() {
   const toggleDarkMode = () => {
     setIsDarkMode(!isDarkMode);
   };
+
+  useEffect(() => {
+    const handleColorSchemeChange = (e: MediaQueryListEvent) => {
+      setIsDarkMode(e.matches);
+    };
+
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+    mediaQuery.addEventListener("change", handleColorSchemeChange);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleColorSchemeChange);
+    };
+  }, []);
 
   useEffect(() => {
     document.documentElement.style.setProperty(
@@ -27,11 +43,9 @@ function App() {
   }, [selectedFont]);
 
   return (
-    <div className={`w-screen  ${isDarkMode ? "dark" : ""}`}>
+    <div className={`w-screen ${isDarkMode ? "dark" : ""}`}>
       <div className="w-[100%] flex items-center justify-center dark:bg-darkest">
-        <div
-          className={`w-[90vw] md:w-[88vw] lg:w-[60vw] py-2 md:py-4 lg:py-8 bg-lighter dark:bg-darkest`}
-        >
+        <div className={`w-[90vw] md:w-[88vw] lg:w-[60vw] py-2 md:py-4 lg:py-8 bg-lighter dark:bg-darkest`}>
           <Header
             selectedFont={selectedFont}
             onFontChange={handleFontChange}
