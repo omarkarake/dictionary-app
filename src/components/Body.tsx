3ddb49045a926e68bb950784f66746184330f885
omar karake <omar.karake@amalitechtraining.org>
2024-06-28 12:12:05 +0200

    updating the flow of playing the audio when searched, still implementing

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { RiSearchLine } from "react-icons/ri";
+import { IoMdPlay } from "react-icons/io";
 
 const Body = () => {
   const [inputValue, setInputValue] = useState("");
@@ -23,31 +24,45 @@ const Body = () => {
 
   return (
     <>
-      <div className="mt-6 md:mt-[51px]">
-        <div
-          className={`w-[100%] h-12 py-4 px-6 md:p-6 flex justify-between items-center bg-light dark:bg-darker rounded-2xl ${
-            borderColor ? `border-[1px] ${borderColor}` : ""
-          }`}
-        >
-          <input
-            type="text"
-            className="w-[250px] md:w-[400px] font-bold text-[20px] outline-none border-none bg-transparent text-dark placeholder:opacity-25"
-            placeholder="Search for any word…"
-            onChange={handleInputChange}
-            value={inputValue}
-          />
-          <RiSearchLine
-            className="w-4 h-4 text-purple cursor-pointer"
-            onClick={handleSearchClick}
-          />
+      <div>
+        <div className="mt-6 md:mt-[51px]">
+          <div
+            className={`w-[100%] h-12 py-4 px-6 md:p-6 flex justify-between items-center bg-light dark:bg-darker rounded-2xl ${
+              borderColor ? `border-[1px] ${borderColor}` : ""
+            }`}
+          >
+            <input
+              type="text"
+              className="w-[250px] md:w-[400px] font-bold text-[20px] dark:text-lighter outline-none border-none bg-transparent text-dark placeholder:opacity-25"
+              placeholder="Search for any word…"
+              onChange={handleInputChange}
+              value={inputValue}
+            />
+            <RiSearchLine
+              className="w-4 h-4 text-purple cursor-pointer"
+              onClick={handleSearchClick}
+            />
+          </div>
+          {borderColorRed ? (
+            <p className="text-body-s md:text-heading-s text-red mt-2">
+              Whoops, can’t be empty…
+            </p>
+          ) : (
+            ""
+          )}
         </div>
-        {borderColorRed ? (
-          <p className="text-heading-s text-red mt-2">
-            Whoops, can’t be empty…
-          </p>
-        ) : (
-          ""
-        )}
+        <div>
+          <div>
+            <p>keyboard</p>
+            <p>/ˈkiːbɔːd/</p>
+          </div>
+          <div
+            className="h-12 w-12 flex justify-center items-center rounded-full"
+            style={{ backgroundColor: "rgba(164, 69, 237, 0.25)" }}
+          >
+            <IoMdPlay className="w-[13px] h-[13px] text-purple cursor-pointer" />
+          </div>
+        </div>
       </div>
     </>
   );
